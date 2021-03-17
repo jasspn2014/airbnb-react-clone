75cc30c9902a30c2062df134941b305fc2061fa4
Jasmeet Singh Arora <jasspn2014@gmail.com>
2021-03-17 19:02:44 +0530

    Removed unwanted files and added header component and added home component

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../Banner/Banner";
+import "./HomePage.css";
+
+function HomePage() {
+  return (
+    <div className="home">
+      <h1>Home Component</h1>
+      <Banner />
+    </div>
+  );
+}
+
+export default HomePage;
